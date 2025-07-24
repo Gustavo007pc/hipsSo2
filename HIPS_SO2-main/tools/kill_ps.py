f3757ff3cbebe8b2f784ea346439a77a43decbde
Gustavo007pc <70350154+Gustavo007pc@users.noreply.github.com>
2025-07-24 09:03:09 -0300

    Delete HIPS_SO2-main directory

--- HIPS_SO2-main/tools/kill_ps.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-def kill_process(pid):
-    try:
-        subprocess.run(["sudo", "kill", "-9", str(pid)])
-        print(f"Proceso {pid} terminado.")
-    except:
-        print(f"Error al matar el proceso {pid}. Ya ha sido terminado o no existe.")
